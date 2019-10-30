94a23774232a993be425ee692e17128a994f7e73
Daniel Burckhardt <daniel.burckhardt@sur-gmbh.ch>
2019-10-30 15:21:25 -0400

    Switch /upload to dropzone

--- src/Controller/WordToTeiController.php
+++ src/Controller/WordToTeiController.php
@@ -20,49 +20,62 @@ extends Controller
      */
     public function uploadAction(Request $request)
     {
-        // replace this example code with whatever you need
+        if ($request->isMethod('post')) {
+            $file = $request->files->get('file');
+
+            if (is_null($file)) {
+                $request->getSession()
+                        ->getFlashBag()
+                        ->add('warning', 'No upload found, please try again')
+                    ;
+            }
+            else {
+                $mime = $file->getMimeType();
+                if (!in_array($mime, [
+                        'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+                        'application/docx',
+                    ]))
+                {
+                    $request->getSession()
+                            ->getFlashBag()
+                            ->add('error', "Uploaded file wasn't recognized as a Word-File (.docx)")
+                        ;
+                }
+                else {
+                    $officeDoc = new \App\Utils\BinaryDocument();
+                    $officeDoc->load($file->getRealPath());
+
+                    $pandocConverter = $this->get(\App\Utils\PandocConverter::class);
+
+                    // inject TeiFromWordCleaner
+                    $myTarget = new class()
+                    extends \App\Utils\TeiSimplePrintDocument
+                    {
+                        use \App\Utils\TeiFromWordCleaner;
+                    };
+
+                    $pandocConverter->setOption('target', $myTarget);
+
+                    $teiSimpleDoc = $pandocConverter->convert($officeDoc);
+
+                    $conversionOptions = [
+                        'prettyPrinter' => $this->get('app.tei-prettyprinter'),
+                        'language' => \App\Utils\Iso639::code1to3($request->getLocale()),
+                        'genre' => 'document', // todo: make configurable
+                    ];
+
+                    $converter = new \App\Utils\TeiSimplePrintToDtabfConverter($conversionOptions);
+                    $teiDtabfDoc = $converter->convert($teiSimpleDoc);
+
+                    return new Response((string)$teiDtabfDoc, Response::HTTP_OK, [
+                        'Content-Type' => 'text/xml',
+                    ]);
+                }
+            }
+        }
+
+        // render upload form
         return $this->render('Upload/index.html.twig', [
         ]);
-    }
-
-    /**
-     * @Route("/upload/handler", name="upload-handler")
-     */
-    public function wordToTeiAction(Request $request)
-    {
-        $output = [ 'uploaded' => false ];
-
-        $file = $request->files->get('file');
-
-        $officeDoc = new \App\Utils\BinaryDocument();
-        $officeDoc->load($file->getRealPath());
-
-        $pandocConverter = $this->get(\App\Utils\PandocConverter::class);
-
-        // inject TeiFromWordCleaner
-        $myTarget = new class()
-        extends \App\Utils\TeiSimplePrintDocument
-        {
-            use \App\Utils\TeiFromWordCleaner;
-        };
-
-        $pandocConverter->setOption('target', $myTarget);
-
-        $teiSimpleDoc = $pandocConverter->convert($officeDoc);
-
-        $conversionOptions = [
-            'prettyPrinter' => $this->get('app.tei-prettyprinter'),
-            'language' => \App\Utils\Iso639::code1to3($request->getLocale()),
-            'genre' => 'document', // todo: make configurable
-        ];
-
-        $converter = new \App\Utils\TeiSimplePrintToDtabfConverter($conversionOptions);
-        $teiDtabfDoc = $converter->convert($teiSimpleDoc);
-
-        $output['uploaded'] = true;
-        $output['valid'] = $teiDtabfDoc->validate($this->get('kernel')->getProjectDir() . '/data/schema/basisformat.rng');
-        $output['content'] = (string)$teiDtabfDoc;
-
-        return new JsonResponse($output);
     }
 }
