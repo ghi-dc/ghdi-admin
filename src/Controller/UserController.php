42a8adc0f914c19077e7e176d126de71976bcddb
Daniel Burckhardt <daniel.burckhardt@sur-gmbh.ch>
2019-11-05 16:38:55 -0500

    Switch to Symfony Form for login

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 /**
- *
+ * Render a Login form
  */
 class UserController
 extends Controller
@@ -23,15 +23,27 @@ extends Controller
             $authenticationUtils = $this->get('security.authentication_utils');
         }
 
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-
         // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
+        $defaultData = [ '_username' => $authenticationUtils->getLastUsername() ];
+        $form = $this->createFormBuilder($defaultData)
+            ->add('_username', \Symfony\Component\Form\Extension\Core\Type\TextType::class)
+            ->add('_password', \Symfony\Component\Form\Extension\Core\Type\PasswordType::class)
+            ->add('Login', \Symfony\Component\Form\Extension\Core\Type\SubmitType::class)
+            ->getForm()
+            ;
+
+        // set the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        if (!empty($error)) {
+            $form->addError(new \Symfony\Component\Form\FormError(
+                $error->getMessageKey()
+            ));
+        }
+
+        $form->handleRequest($request);
 
         return $this->render('User/login.html.twig', [
-            'last_username' => $lastUsername,
-            'error'         => $error,
+            'form' => $form->createView(),
         ]);
     }
 
