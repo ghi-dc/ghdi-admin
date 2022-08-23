cccc1d170c22191e54dfa8cacb87a7b2bd7cef80
Daniel Burckhardt <daniel.burckhardt@sur-gmbh.ch>
2022-08-23 14:50:42 +0200

    Pass tempDir outside vendor to Mpdf

--- src/Utils/PdfGenerator.php
+++ src/Utils/PdfGenerator.php
@@ -8,8 +8,21 @@ extends \Mpdf\Mpdf
     // mpdf
     public function __construct($options = [])
     {
-        // mpdf 7.x
+        // mpdf >= 7.x
         $defaultConfig = (new \Mpdf\Config\ConfigVariables())->getDefaults();
+
+        /*
+         * mPDF is pre-configured to use <path to mpdf>/tmp as a directory
+         * to write temporary files (mainly for images).
+         * Write permissions must be set for read/write access for the tmp directory.
+         *
+         * As the default temp directory will be in vendor folder,
+         * is is advised to set custom temporary directory.
+         */
+        $options['tempDir'] = array_key_exists('tempDir', $options)
+            ? $options['tempDir']
+            : sys_get_temp_dir();
+
         $fontDir = $defaultConfig['fontDir'];
 
         $options['fontDir'] = array_key_exists('fontDir', $options)
