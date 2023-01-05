a52075594dd70e4020b2d3eab762a16b740f5635
Daniel Burckhardt <daniel.burckhardt@sur-gmbh.ch>
2023-01-05 13:52:59 +0100

    Switch to RoutingConfigurator

--- src/MicroKernel.php
+++ src/MicroKernel.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel;
-use Symfony\Component\Routing\RouteCollectionBuilder;
+use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 // see https://symfony.com/doc/current/configuration/micro_kernel_trait.html
 final class MicroKernel
@@ -38,7 +38,7 @@ extends Kernel
     /*
      * {@inheritDoc}
      */
-    public function registerBundles()
+    public function registerBundles() : iterable
     {
         $bundles = [
             new \Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
@@ -68,7 +68,7 @@ extends Kernel
             new \FS\SolrBundle\FSSolrBundle(),
         ];
 
-        if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
+        if (in_array($this->getEnvironment(), [ 'dev', 'test' ], true)) {
             $bundles[] = new \Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new \Symfony\Bundle\DebugBundle\DebugBundle();
         }
@@ -86,19 +86,19 @@ extends Kernel
     }
 
     // see https://github.com/symfony/symfony-standard/blob/master/app/AppKernel.php
-    public function getCacheDir()
+    public function getCacheDir() : string
     {
-        return $this->getProjectDir().'/var/cache/'.$this->getEnvironment();
+        return $this->getProjectDir() . '/var/cache/' . $this->getEnvironment();
     }
 
-    public function getLogDir()
+    public function getLogDir() : string
     {
-        return $this->getProjectDir().'/var/logs';
+        return $this->getProjectDir() . '/var/logs';
     }
 
-    public function getConfigDir()
+    public function getConfigDir() : string
     {
-        return $this->getProjectDir().'/config';
+        return $this->getProjectDir() . '/config';
     }
 
     /*
@@ -106,8 +106,8 @@ extends Kernel
      */
     protected function configureContainer(ContainerBuilder $c, LoaderInterface $loader)
     {
-        $loader->load($this->getConfigDir().'/config_'.$this->getEnvironment().'.yaml');
-        $loader->load($this->getConfigDir().'/services.yaml');
+        $loader->load($this->getConfigDir() . '/config_' . $this->getEnvironment() . '.yaml');
+        $loader->load($this->getConfigDir() . '/services.yaml');
 
         // configure WebProfilerBundle only if the bundle is enabled
         // see https://symfony.com/doc/current/configuration/micro_kernel_trait.html#advanced-example-twig-annotations-and-the-web-debug-toolbar
@@ -127,18 +127,18 @@ extends Kernel
      * to show all your routes
      *
      */
-    protected function configureRoutes(RouteCollectionBuilder $routes)
+    protected function configureRoutes(RoutingConfigurator $routes)
     {
-        if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
-            $routes->import('@WebProfilerBundle/Resources/config/routing/wdt.xml', '/_wdt');
-            $routes->import('@WebProfilerBundle/Resources/config/routing/profiler.xml', '/_profiler');
+        if (in_array($this->getEnvironment(), [ 'dev', 'test' ], true)) {
+            $routes->import('@WebProfilerBundle/Resources/config/routing/wdt.xml')->prefix('/_wdt');
+            $routes->import('@WebProfilerBundle/Resources/config/routing/profiler.xml')->prefix('/_profiler');
 
             // Preview error pages through /_error/{statusCode}
             //   see http://symfony.com/doc/current/cookbook/controller/error_pages.html
-            $routes->import('@FrameworkBundle/Resources/config/routing/errors.xml', '/_error');
+            $routes->import('@FrameworkBundle/Resources/config/routing/errors.xml')->prefix('/_error');
         }
 
-        // our controllers
-        $routes->import($this->getConfigDir().'/routes.yaml');
+        // App controllers
+        $routes->import($this->getConfigDir() . '/routes.yaml');
     }
 }
