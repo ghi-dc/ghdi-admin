0c3dc33d6b915e6539e585ed4db76eb7674c2f5c
Daniel Burckhardt <daniel.burckhardt@sur-gmbh.ch>
2024-01-04 09:41:55 +0100

    Address deprecations

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -7,12 +7,12 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\KernelInterface;
-use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Routing\Annotation\Route;
 
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 
 use App\Service\ExistDbClientService;
 use App\Utils\XmlPrettyPrinter\XmlPrettyPrinter;
--- src/Controller/CollectiveAccessController.php
+++ src/Controller/CollectiveAccessController.php
@@ -8,9 +8,10 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Security;
 
 use Symfony\Contracts\Translation\TranslatorInterface;
+
+use Symfony\Bundle\SecurityBundle\Security;
 
 use Asika\Autolink\Linker;
 
