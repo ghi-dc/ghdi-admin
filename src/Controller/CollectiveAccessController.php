96e6ba4aa1bb3b24c4297f7a874750c55cc7f066
Daniel Burckhardt <daniel.burckhardt@sur-gmbh.ch>
2024-03-09 13:50:48 +0100

    Remove use statements

--- src/Controller/CollectiveAccessController.php
+++ src/Controller/CollectiveAccessController.php
@@ -5,15 +5,12 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 use Symfony\Bundle\SecurityBundle\Security;
-
-use Asika\Autolink\Linker;
 
 use Cocur\Slugify\SlugifyInterface;
 
