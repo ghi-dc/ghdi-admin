04c0974e1d5e7a0d1217e9202172b69a7e95b323
Daniel Burckhardt <daniel.burckhardt@sur-gmbh.ch>
2025-03-02 12:02:12 +0100

    Add missing use

--- src/Security/FormLoginAuthenticator.php
+++ src/Security/FormLoginAuthenticator.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
