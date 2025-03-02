7b9c7c81cf5d259255358597bb010f123d804dfe
Daniel Burckhardt <daniel.burckhardt@sur-gmbh.ch>
2025-03-02 12:03:03 +0100

    Remove deprecated method

--- src/Security/HttpBasicAuthenticator.php
+++ src/Security/HttpBasicAuthenticator.php
@@ -15,7 +15,6 @@ use Symfony\Component\Security\Http\Authenticator\AuthenticatorInterface;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\CustomCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
-use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
 use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
 
 class HttpBasicAuthenticator implements AuthenticatorInterface, AuthenticationEntryPointInterface
@@ -65,16 +64,6 @@ class HttpBasicAuthenticator implements AuthenticatorInterface, AuthenticationEn
         );
 
         return $passport;
-    }
-
-    /**
-     * @deprecated since Symfony 5.4, use {@link createToken()} instead
-     */
-    public function createAuthenticatedToken(PassportInterface $passport, string $firewallName): TokenInterface
-    {
-        trigger_deprecation('symfony/security-http', '5.4', 'Method "%s()" is deprecated, use "%s::createToken()" instead.', __METHOD__, __CLASS__);
-
-        return $this->createToken($passport, $firewallName);
     }
 
     public function createToken(Passport $passport, string $firewallName): TokenInterface
